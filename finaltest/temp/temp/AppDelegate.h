e2a61f9f421cd4e5f7f92f4868132d01aa8fd57f
Mingfei Yan <mingfeiy@chadhan-l01.redmond.corp.microsoft.com>
2012-12-06 14:23:20 -0800

    rest

---
+++ finaltest/temp/temp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  temp
+//
+//  Created by Mingfei Yan on 12/6/12.
+//  Copyright (c) 2012 Mingfei Yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
